9b7c6e69b466780cbd79e4fd0121555acdc76340
Siddharth Kannan <siddharthkannan@tutanota.com>
2016-02-29 01:07:50 +0530

    Add the signout button, show only articles
    - shared by the signed in user
    
    Signed-off-by: Siddharth Kannan <siddharthkannan@tutanota.com>

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,9 @@
 class UserController < ApplicationController
   def index
-		@allArticles = Article.all
+		if not user_signed_in?
+			redirect_to root_path, alert: "You need to be logged in to use that feature"
+		end
+
+		@allArticles = current_user.articles
   end
 end
