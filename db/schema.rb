58d44dce470e79074dfa00844cf341a93e51274a
Siddharth Kannan <siddharthkannan@tutanota.com>
2016-03-06 16:37:01 +0530

    Add confirmable to the users model
    - part of last commit, raised error when pushed to production
    
    Signed-off-by: Siddharth Kannan <siddharthkannan@tutanota.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228192041) do
+ActiveRecord::Schema.define(version: 20160306110412) do
 
   create_table "articles", force: true do |t|
     t.string   "link"
@@ -38,8 +38,12 @@ ActiveRecord::Schema.define(version: 20160228192041) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
