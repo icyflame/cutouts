b1816e71647e17a155c565077a52a89aa94d4f73
Siddharth Kannan <siddharthkannan@tutanota.com>
2016-03-12 19:24:12 +0530

    Make username unique from rake
    Signed-off-by: Siddharth Kannan <siddharthkannan@tutanota.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306110412) do
+ActiveRecord::Schema.define(version: 20160312135338) do
 
   create_table "articles", force: true do |t|
     t.string   "link"
@@ -46,5 +46,6 @@ ActiveRecord::Schema.define(version: 20160306110412) do
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
