f3f5fadc7492cff78d8db85cdefd5448198d5b9d
Shawn <sjcovell@users.noreply.github.com>
2016-10-08 20:51:26 -0700

    Update user_controller.rb
    formatting

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,8 @@
 class UserController < ApplicationController
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def index
-		@allArticles = current_user.articles.last(10).reverse
+    @allArticles = current_user.articles.last(10).reverse
   end
 
   def export_articles
