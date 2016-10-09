fafbf504f82005a0276af810f593c2f94940c027
Shawn Covell <covell.sj@gmail.com>
2016-10-09 14:00:46 -0700

    Add functionality to export articles to human-readable HTML

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,9 +6,15 @@ class UserController < ApplicationController
   end
 
   def export_articles
+    @articles = current_user.articles.all
+
     respond_to do |format|
+      format.html do 
+        send_data(render_to_string(partial: "user/export_articles", layout: false, articles: @articles), type: 'text/html', filename: "cutout-articles-#{Date.today}.html", disposition: 'attachment')
+      end
+
       format.json do
-        articles = JSON.pretty_generate(current_user.articles.all.as_json(only: [:link, :quote, :author]))
+        articles = JSON.pretty_generate(@articles.as_json(only: [:link, :quote, :author]))
         send_data(articles, type: 'application/json', filename: "cutout-articles-#{Date.today}.json", disposition: 'attachment')
       end
     end
