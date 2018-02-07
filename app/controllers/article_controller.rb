c5a2c84b928ecfb958993f76750cff5d15a632d0
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-02-07 22:55:10 +0530

    ensure that show page is accessibly without login

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,5 +1,5 @@
 class ArticleController < ApplicationController
-	before_filter :authenticate_user!
+	before_filter :authenticate_user!, :except => [:show]
 
 	def index
 		if params[:input].match(/^tag:/i)
