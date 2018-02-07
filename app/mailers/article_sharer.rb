49859156c2693b274edb9c2bcecf6556afaee70a
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-02-07 23:18:28 +0530

    include article title in email subject whenever possible

--- app/mailers/article_sharer.rb
+++ app/mailers/article_sharer.rb
@@ -4,6 +4,7 @@ class ArticleSharer < ActionMailer::Base
   def share_article(article, emails, from_user)
     @article = article
     @from_username = from_user.username
-    mail(:to => emails, :subject => "#{from_user.username} shared a cutout with you!")
+    subject_component = (article.title && article.title.length > 0) ? article.title : "a cutout"
+    mail(:to => emails, :subject => "#{from_user.username} has shared #{subject_component} with you!")
   end
 end
