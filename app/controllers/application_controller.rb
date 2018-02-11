0afbd193009499ca1eb3212f93a38f77ede9d3cc
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-02-11 12:13:47 +0530

    parameter sanitizer API changed at devise 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,8 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
-		devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password) }
-		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+		devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
+		devise_parameter_sanitizer.permit(:account_update, keys: [ :username ])
 	end
 
   def is_valid_email text
