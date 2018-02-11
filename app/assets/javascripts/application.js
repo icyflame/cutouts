da4266358930d756b9287bb6e387d8b1af17083e
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-02-11 15:21:18 +0530

    include popper and concatenated version of bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
+//= require popper
+//= require bootstrap
 //= require jquery_ujs
 //= require jquery-ui
 //= require turbolinks
