c78e5067c202055d07ec8d331d02a1883dfc7942
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-02-11 19:32:04 +0530

    add tests for the user model

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,28 +1,27 @@
 require 'test_helper'
+include ArticleHelper
 
 class ArticleTest < ActiveSupport::TestCase
-  test "should not save article without link, author and quote" do
+  test "should not save article without link and quote" do
     a = Article.new
-    assert_not a.save
-  end
-
-  test "should not save article without author and quote" do
-    a = Article.new
-    a.link = "http://example.com"
     assert_not a.save
   end
 
   test "should not save article without quote" do
     a = Article.new
     a.link = "http://example.com"
-    a.author = "Amy Dunne"
     assert_not a.save
   end
 
-  test "should save article with link, author and quote (bare minimum)" do
+  test "should not save article without link" do
+    a = Article.new
+    a.quote = "some quote"
+    assert_not a.save
+  end
+
+  test "should save article with valid link and quote (bare minimum)" do
     a = Article.new
     a.link = "http://example.com"
-    a.author = "Amy Dunne"
     a.quote = "We are so cute, I wanna punch us in the face"
     assert a.save
   end
@@ -33,5 +32,19 @@ class ArticleTest < ActiveSupport::TestCase
     a.author = "Amy Dunne"
     a.quote = "We are so cute, I wanna punch us in the face"
     assert_not a.save
+  end
+
+  test "should save article with valid, link, quote, author, tags as a public article" do
+    a = Article.new
+    a.link = "http://example.com"
+    a.quote = "testing"
+    a.author = "jack"
+    a.tags = "test1, test2"
+
+    assert a.valid?
+    assert a.save
+
+    assert_equal a.tags_array.count, 2
+    assert_equal viz_int_val(a.visibility), 0
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user can't be created without email" do
+    a = User.new
+    a.username = "test"
+    assert_not a.save
+  end
+
+  test "user can't be created without username" do
+    a = User.new
+    a.email = "test@example.com"
+    assert_not a.save
+  end
+
+  test "user can be created with just username and email" do
+    a = User.new
+    a.username = "test"
+    a.email = "test@example.com"
+  end
+
+  test "user can't be created with a duplicate email ID" do
+    a = User.new
+    a.username = "test"
+    a.email = "carson@downton.com"
+    assert_not a.save
+  end
+
+  test "user can't be created with a duplicate username" do
+    a = User.new
+    a.username = "carson"
+    a.email = "carons1@downton.com"
+    assert_not a.save
+  end
+
+  test "user is associated with some articles" do
+    carson = User.where({ :username => "carson" }).first
+    assert_equal carson.articles.count, 2
+  end
 end
