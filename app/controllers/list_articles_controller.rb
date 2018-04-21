9f3b1c11f327197ed85eecc1b906723b03f6dab4
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-04-21 09:26:18 +0530

    don't show own cutouts in feed
    fix #27

--- app/controllers/list_articles_controller.rb
+++ app/controllers/list_articles_controller.rb
@@ -8,7 +8,7 @@ class ListArticlesController < ApplicationController
     @page = params[:page] ? params[:page].to_i : 0
     @page = @page >= 0 ? @page : 0
 
-    @articles = Article.where({ :visibility => 0 }).limit(20).offset(@page * 20)
+    @articles = Article.where({ :visibility => 0 }).where.not({ user_id: current_user.id }).limit(20).offset(@page * 20)
     @page = @page + 1
 
     # decide whether to show the next page
