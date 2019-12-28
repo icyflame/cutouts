5f88b547fc73c6bdc038d111440852b74ff69e85
Siddharth Kannan <mail@siddharthkannan.in>
2019-12-28 15:25:52 +0530

    Convert to json explicitly; next migration changes the type anyway

--- db/migrate/20161014194032_add_tags_to_articles.rb
+++ db/migrate/20161014194032_add_tags_to_articles.rb
@@ -1,5 +1,5 @@
 class AddTagsToArticles < ActiveRecord::Migration[4.2]
   def change
-  	add_column :articles, :tags, :string, default: [], array: true
+    add_column :articles, :tags, :string, default: [].to_json, array: true
   end
 end
