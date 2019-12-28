41038fc113df40719971466e78e9b4091a66a03c
Siddharth Kannan <mail@siddharthkannan.in>
2019-12-28 15:18:33 +0530

    Move array: true to the end of the migration

--- db/migrate/20161014194032_add_tags_to_articles.rb
+++ db/migrate/20161014194032_add_tags_to_articles.rb
@@ -1,5 +1,5 @@
 class AddTagsToArticles < ActiveRecord::Migration[4.2]
   def change
-  	add_column :articles, :tags, :string, array: true, default: []
+  	add_column :articles, :tags, :string, default: [], array: true
   end
 end
