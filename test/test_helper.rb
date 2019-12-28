4ab703ad0daa01fb948471423836cdc1d8bbc4a6
Siddharth Kannan <mail@siddharthkannan.in>
2019-12-28 12:58:28 +0530

    Change function call to explicitly pass Hash
    2.7.0 is more strict about the conversion between positional and named
    parameters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,9 +12,9 @@ class ActiveSupport::TestCase
     factory :user do
       email { Faker::Internet.email }
       username { Faker::Lorem.words(3).join "_" }
-      password "password"
-      password_confirmation "password"
-      confirmed_at Date.today
+      password { "password" }
+      password_confirmation { "password" }
+      confirmed_at { Date.today }
     end
   end
 
